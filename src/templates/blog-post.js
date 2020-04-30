66c7e7371f51a624a73bb668ecb1d8af1693bfa8
harshthakur066 <harshthakur066@gmail.com>
2020-04-30 14:38:26 +0530

    added individual blog page dynamically by querrying in graphQL

---
+++ src/templates/blog-post.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <SEO title={`/post/${post.id}`} />
+      <div>
+        <Link to="/blog">Go Back</Link>
+        <hr />
+        <h1>{post.frontmatter.title}</h1>
+        <h4>
+          Posted by {post.frontmatter.author} on {post.frontmatter.date}
+        </h4>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      id
+      frontmatter {
+        path
+        title
+        author
+        date
+      }
+    }
+  }
+`
+
+export default IndexPage
